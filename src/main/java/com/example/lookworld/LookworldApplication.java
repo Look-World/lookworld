cd32468aba240111c9b038eb6c623d51fb8cd349
lookworld <1435127336@qq.com>
2021-11-21 16:11:43 +0800

    初始化框架

---
+++ src/main/java/com/example/lookworld/LookworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.lookworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LookworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lookworld/LookworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lookworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
